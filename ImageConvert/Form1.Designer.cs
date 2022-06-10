44a350a64d61d3220076f36e4464ae095ae30006
The_Best_T <zhamichenok@gmail.com>
2022-06-11 01:47:39 +0300

    Add project files.

---
+++ ImageConvert/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace ImageConvert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputButton = new System.Windows.Forms.Button();
+            this.outputButton = new System.Windows.Forms.Button();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.inputFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.outputFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // inputButton
+            // 
+            this.inputButton.Location = new System.Drawing.Point(109, 188);
+            this.inputButton.Name = "inputButton";
+            this.inputButton.Size = new System.Drawing.Size(138, 60);
+            this.inputButton.TabIndex = 0;
+            this.inputButton.Text = "Input";
+            this.inputButton.UseVisualStyleBackColor = true;
+            this.inputButton.Click += new System.EventHandler(this.inputButton_Click);
+            // 
+            // outputButton
+            // 
+            this.outputButton.Location = new System.Drawing.Point(510, 188);
+            this.outputButton.Name = "outputButton";
+            this.outputButton.Size = new System.Drawing.Size(138, 60);
+            this.outputButton.TabIndex = 1;
+            this.outputButton.Text = "Output";
+            this.outputButton.UseVisualStyleBackColor = true;
+            this.outputButton.Click += new System.EventHandler(this.outputButton_Click);
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(311, 188);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(138, 60);
+            this.convertButton.TabIndex = 2;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.outputButton);
+            this.Controls.Add(this.inputButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button inputButton;
+        private System.Windows.Forms.Button outputButton;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.FolderBrowserDialog inputFolder;
+        private System.Windows.Forms.FolderBrowserDialog outputFolder;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+    }
+}
+
---
+++ ImageConvert/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ImageConvert
+{
+    public partial class Form1 : Form
+    {
+        private string selectedFileName = "";
+        private string selectedFolderPath = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void inputButton_Click(object sender, EventArgs e)
+        {
+            openFileDialog.Filter = "JPG images (*.JPG)|*.jpg";
+            openFileDialog.ShowDialog();
+            selectedFileName = openFileDialog.FileNames[0];
+        }
+
+        private bool InvalidData()
+        {
+            string errorMessage = "";
+
+            if (String.IsNullOrEmpty(selectedFolderPath))
+            {
+                errorMessage = "Result Path not selected!";
+            }
+            if (String.IsNullOrEmpty(selectedFileName))
+            {
+                errorMessage = "File not selected!";
+            }
+            if (!String.IsNullOrEmpty(errorMessage))
+            {
+                MessageBox.Show(
+                   errorMessage,
+                   "Error",
+                   MessageBoxButtons.OK,
+                   MessageBoxIcon.Error
+                   );
+                return false;
+            }
+            return true;
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            if (!InvalidData())
+                return;
+
+            Bitmap image = new Bitmap(selectedFileName);
+
+            string newFilePath = selectedFolderPath + "//" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+            using (StreamWriter writer = new StreamWriter(newFilePath))
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    for (int i = 0; i < image.Width; i++)
+                    {
+                        UInt32 pixel = (UInt32)(image.GetPixel(i, j).ToArgb());
+                        float R = (pixel & 0x00FF0000) >> 16;
+                        float G = (pixel & 0x0000FF00) >> 8;
+                        float B = pixel & 0x000000FF;
+                        R = G = B = (R + G + B) / 3.0f;
+                        UInt32 newPixel = 0xFF000000 | ((UInt32)R << 16) | ((UInt32)G << 8) | ((UInt32)B);
+
+                        if (newPixel < 4281545523)
+                        {
+                            writer.Write('#');
+                        }
+                        else if (newPixel >= 4281545523 && newPixel < 4284900966)
+                        {
+                            writer.Write('0');
+                        }
+                        else if (newPixel >= 4284900966 && newPixel < 4288256409)
+                        {
+                            writer.Write('*');
+                        }
+                        else if (newPixel >= 4288256409 && newPixel < 4291611852)
+                        {
+                            writer.Write('.');
+                        }
+                        else
+                        {
+                            writer.Write(' ');
+                        }
+                    }
+                    writer.WriteLine();
+                }
+            }
+        }
+
+        private void outputButton_Click(object sender, EventArgs e)
+        {
+            outputFolder.ShowDialog();
+            selectedFolderPath = outputFolder.SelectedPath;
+        }
+    }
+}
